818e54c5228ca8b4ed621ec1d9fc2b9cd7f5875e
Akatofff <super.kostya.a@yandex.ru>
2025-04-09 22:50:44 +0300

    Algorithms first commit.

---
+++ src/main/java/local/algorithm/Main.java
@@ -0,0 +1,17 @@
+package local.algorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str1 = "abcabcaaa";
+        String str2 = "abcbsdv";
+        String str3 = "aaaaaaaa";
+
+
+        System.out.println(LongestSubstring.lengthOfLongestSubstring(str1));
+        System.out.println(LongestSubstring.lengthOfLongestSubstring(str2));
+        System.out.println(LongestSubstring.lengthOfLongestSubstring(str3));
+
+    }
+
+}
